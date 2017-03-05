7129221e2db8947176485c07ecb70ec2c8474c5d
codekissyoung <cky951010@163.com>
2017-03-05 11:52:07 +0800

    a

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argv,char* argc[]){
 	printf("%d \n",max(4,6)); // 调用函数
 	printf("argv is %d \n",argv); // 程序输入参数的个数
 	for(int i = 0;i < argv;i++){
-		printf("argc[%d] is %s \n",i,argc[i]);　// 依次打印输入的参数
+		printf("argc[%d] is %s \n",i,argc[i]);// 依次打印输入的参数
 	}
 	return 0;
 }
