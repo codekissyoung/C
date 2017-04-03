c62bd093aa5398be241eb294c636aebcdaf93022
codekissyoung <cky951010@163.com>
2017-04-03 16:37:02 +0800

    add files

--- main.c
+++ main.c
@@ -24,107 +24,127 @@ int main(int argv,char* argc[]){
 	int ar2[3][2];
 	int *pp;
 	
-	printf("*argc[1] : %s \n",argc[1]);
 
 	switch(*argc[1]){
+
 		case 'a':
 			printf("输入了a\n");
 			break;
+		
+		// 指向多维数组的指针
+		case 'b':/*{{{*/
+			int zippo[3][2] =
+			{
+				{2,3},
+				{4,5},
+				{6,7}
+			};
+			int (*pz)[2]; // 指向一个含有两个int类型值的数组
+			pz = zippo;
+			printf("zippo : %p , zippo[0]: %p :zippo[0][0] : %d \n",zippo,zippo[0],zippo[0][0]);
+			printf("pz : %p , *pz: %p : **pz: %d \n",pz,*pz,**pz);
+			printf("pz = %p,pz + 1:%p \n",pz,pz+1);
+			printf("pz[0] = %p,pz[0] + 1:%p \n",pz[0],pz[0]+1);
+			printf("*pz = %p,*pz + 1:%p \n",*pz,*pz+1);
+			printf("**pz = %d,*(*pz + 1):%d \n",**pz,*(*pz+1));
+			printf("**(pz + 1) = %d,*(*(pz + 1) + 1):%d \n",**(pz + 1),*(*(pz + 1)+1));
+			printf("pz[0][0] = %d,pz[0][1]:%d \n",pz[0][0],pz[0][1]);
+			printf("-----------------\n\n");
+			break;/*}}}*/
 
+		// 测试EOF
+		case 'c':/*{{{*/
+			char test_eof;
+			while( (test_eof = getchar()) != EOF){
+				putchar(test_eof);
+			}
+			break;/*}}}*/
+	
+		// 测试fgets()
+		case 'd':/*{{{*/
+			char words[STLEN];
+			int i;
+			while(fgets(words,STLEN,stdin) != NULL && words[0] != '\n'){
+				i = 0;
+				while(words[i] != '\n' && words[i] != '\0'){
+					i++;
+				}
+				if(words[i] == '\n'){
+					words[i] = '\0';
+				}else{
+					while(getchar() != '\n'){
+						continue;
+					}
+				}
+				fputs(words,stdout);
+			}
+			break;/*}}}*/
+		
+		// 测试下字符串数组
+		case 'e':/*{{{*/
+			const char *pointer_str[5] = {
+				"string1 heheh",
+				"string2 ,sdfadf",
+				"string3 hdhdhdh",
+				"xxixixixi",
+				"codsdadfssss"
+			};
+			char array_str[5][40] = {
+				"sdfaiisisis",
+				"xixixiix sss",
+				"hahah",
+				"codekissyoung"
+			};
+			for(int k = 0;k < 5;k++){
+				printf("pointer_str[%d] : %p : %s \n",k,pointer_str[k],pointer_str[k]);
+			}
+			for(int p = 0;p < 5;p++){
+				printf("array_str[%d] : %p : %s \n",p,array_str[p],array_str[p]);
+			}
+			break;/*}}}*/
+
+		// 数组字符串和指针字符串的区别
+		case 'f':/*{{{*/
+			char ar[] = MSG;
+			char *pt = MSG;
+			printf("MSG : %p \n",MSG); // 字面量存储的位置
+			printf("ar : %p \n",ar); // 数组存储的位置
+			printf("pt : %p \n",pt); // 指针指向的位置
+			printf("-----------------\n\n");
+			break;/*}}}*/
+
+		case 'g':
+			break;
+
+		case 'h':
+			break;
+		
+		case 'i':
+			break;
+		
+		case 'j':
+			break;
+		
+		case 'k':
+			break;
+		
 		default:
 			printf("运行　./cky n (n 为任意数字)\n");
 			break;
 	}
 
 
-	// 指向多维数组的指针
-	int zippo[3][2] =
-	{
-		{2,3},
-		{4,5},
-		{6,7}
-	};
-	int (*pz)[2]; // 指向一个含有两个int类型值的数组
-	pz = zippo;
-	printf("zippo : %p , zippo[0]: %p :zippo[0][0] : %d \n",zippo,zippo[0],zippo[0][0]);
-	printf("pz : %p , *pz: %p : **pz: %d \n",pz,*pz,**pz);
-	printf("pz = %p,pz + 1:%p \n",pz,pz+1);
-	printf("pz[0] = %p,pz[0] + 1:%p \n",pz[0],pz[0]+1);
-	printf("*pz = %p,*pz + 1:%p \n",*pz,*pz+1);
-	printf("**pz = %d,*(*pz + 1):%d \n",**pz,*(*pz+1));
-	printf("**(pz + 1) = %d,*(*(pz + 1) + 1):%d \n",**(pz + 1),*(*(pz + 1)+1));
-	printf("pz[0][0] = %d,pz[0][1]:%d \n",pz[0][0],pz[0][1]);
-	printf("-----------------\n\n");
-
-	// 测试EOF
-	/*
-	char test_eof;
-	while( (test_eof = getchar()) != EOF){
-		putchar(test_eof);
-	}
-	*/
-
-	// 测试fgets()
-	char words[STLEN];
-	int i;
-	while(fgets(words,STLEN,stdin) != NULL && words[0] != '\n'){
-		i = 0;
-		while(words[i] != '\n' && words[i] != '\0'){
-			i++;
-		}
-		if(words[i] == '\n'){
-			words[i] = '\0';
-		}else{
-			while(getchar() != '\n'){
-				continue;
-			}
-		}
-		fputs(words,stdout);
-	}
-
-	// 测试下字符串数组
-	const char *pointer_str[5] = {
-		"string1 heheh",
-		"string2 ,sdfadf",
-		"string3 hdhdhdh",
-		"xxixixixi",
-		"codsdadfssss"
-	};
-	char array_str[5][40] = {
-		"sdfaiisisis",
-		"xixixiix sss",
-		"hahah",
-		"codekissyoung"
-	};
-	for(int k = 0;k < 5;k++){
-		printf("pointer_str[%d] : %p : %s \n",k,pointer_str[k],pointer_str[k]);
-	}
-	for(int p = 0;p < 5;p++){
-		printf("array_str[%d] : %p : %s \n",p,array_str[p],array_str[p]);
-	}
-
-	// 数组字符串和指针字符串的区别
-	char ar[] = MSG;
-	char *pt = MSG;
-	printf("MSG : %p \n",MSG); // 字面量存储的位置
-	printf("ar : %p \n",ar); // 数组存储的位置
-	printf("pt : %p \n",pt); // 指针指向的位置
-	printf("-----------------\n\n");
-
 	// 打印其输入的参数
 	for(int i = 0;i < argv;i++){
 		printf("argc[%d] is %s \n",i,argc[i]);// 依次打印输入的参数
 	}
-	printf("-----------------\n\n");
 
 	// 处理多维数组的函数
 	sum_rows(zippo,3);
-	printf("---------------------\n\n");
 
 	// 变长数组的使用
 	int total1 = sum2d(3,2,zippo);
 	printf("total1 : %d \n", total1);
-	printf("---------------------\n\n");
 
 	// 复合字面量
 	int total2 = sum2d( 2 , 2 , (int [2][2]){{2,3},{4,5}});
