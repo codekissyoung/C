136e855023fab6bf0a53cc1307c5c347ae1370de
codekissyoung <cky951010@163.com>
2017-06-12 11:29:31 +0800

    commit

--- include/struct.h
+++ include/struct.h
@@ -1,51 +1,50 @@
-/* 栈 */
-struct stack{
-	int data[1000];
-	int top;
-};
-
-/* 链表 */
-struct node{
-	int data;
-	struct node *next;
-};
-
-// 链表
-struct test_struct{
-	int array[2];
-	char ch;
-};
-
-typedef struct test_struct Test;
-
-/* 队列 */
-struct queue {
-	int data[1000];
-	int head;
-	int tail;
-};
-struct arg_struct{
-	char arg1[10];
-	int arg2;
-	float arg3;
-};
-
-typedef struct arg_struct ARG;
-
-typedef struct heap_stack{
-	int *heap;
-	int *stack;
-} HS;
-
 // 结构化数据
-struct book{
+struct book{/*{{{*/
 	char title[40];
 	char author[40];
 	float value;
-};
+};/*}}}*/
 
-struct msg{
+/* 栈 */
+struct stack{/*{{{*/
+	int data[1000];
+	int top;
+};/*}}}*/
+
+/* 链表 */
+struct node{/*{{{*/
+	int data;
+	struct node *next;
+};/*}}}*/
+
+/* 队列 */
+struct queue {/*{{{*/
+	int data[1000];
+	int head;
+	int tail;
+};/*}}}*/
+
+struct msg{/*{{{*/
 	long msg_types;
 	char msg_buf[511];
-};
+};/*}}}*/
+
+struct test_struct{/*{{{*/
+	int array[2];
+	char ch;
+};/*}}}*/
+
+struct arg_struct{/*{{{*/
+	char arg1[10];
+	int arg2;
+	float arg3;
+};/*}}}*/
+
+typedef struct heap_stack{/*{{{*/
+	int *heap;
+	int *stack;
+} HS;/*}}}*/
+
+typedef struct arg_struct ARG;
+typedef struct test_struct Test;
 
