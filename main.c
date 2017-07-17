8f2b3a22fb986fff0a482eb04329537fe02cccb4
caokaiyan <cky951010@163.com>
2017-07-17 19:16:12 +0800

    update

--- main.c
+++ main.c
@@ -23,6 +23,12 @@ int main(int argc,const char* argv[]){
 		const char *glibc_version = gnu_get_libc_version();
 		printf("cky version : %.2f \n",0.01);
 		printf("glibc version : %s \n",glibc_version);
+		
+		printf("环境变量测试: \n");
+		printf("%s \n",environ[0]); //第一个环境变量
+		printf("%s \n",environ[1]);
+		printf("HOME : %s \n",getenv("HOME")); // 获取环境变量HOME
+
 	}/*}}}*/
 
 	// node 链表操作
@@ -106,9 +112,12 @@ int main(int argc,const char* argv[]){
 			printf("%d\t",test_data[i]);
 		}
 		printf("\n");
-		
 		sort_in_quick(test_data,0,lenth - 1);
-		
+
+		// 第二种实现
+		int arr[] = {30,1,34,53,2,46,5,30,8,9,100,30,28,18,90,78,30,56,84,33,30,20};
+		int arr_size = sizeof(arr) / sizeof(arr[0]);
+		divide(arr,0,arr_size -1);
 		return 0;
 	}/*}}}*/
 
@@ -167,6 +176,14 @@ int main(int argc,const char* argv[]){
 
 	// 线程与进程
 	if(strcmp("thread",argv[1]) == 0){/*{{{*/
+		printf("当前进程ID : %u\n",getpid());
+		printf("当前进程父ID : %u\n",getppid());
+		printf("当前用户ID : %u\n",getuid());
+		printf("当前有效用户ID : %u\n",geteuid());
+		printf("当前组ID : %u\n",getgid());
+		printf("当前有效组ID : %u\n",getegid());
+		printf("-----------------\n\n");
+
 		// 需要传递给线程的参数
 		ARG arg;
 		strcpy(arg.arg1,argv[2]);
@@ -458,7 +475,7 @@ int main(int argc,const char* argv[]){
 	}/*}}}*/
 	
 	// 测试fgets()
-	if("fgets",argv[1]){/*{{{*/
+	if(strcmp("fgets",argv[1]) == 0){/*{{{*/
 		char words[STLEN];
 		int i;
 		while(fgets(words,STLEN,stdin) != NULL && words[0] != '\n'){
@@ -519,243 +536,129 @@ int main(int argc,const char* argv[]){
 		for(int p = 0;p < 5;p++){
 			printf("array_str[%d] : %p : %s \n",p,array_str[p],array_str[p]);
 		}
+
+		char ar[] = MSG;
+		char *pt = MSG;
+		printf("MSG : %p \n",MSG); // 字面量存储的位置
+		printf("ar : %p \n",ar); // 数组存储的位置
+		printf("pt : %p \n",pt); // 指针指向的位置
+		printf("-----------------\n\n");
+		
+		// 处理多维数组的函数
+		int zippo[3][2] =
+		{
+			{2,3},
+			{4,5},
+			{6,7}
+		};
+		sum_rows(zippo,3);
+	
 	}/*}}}*/
 
+	// 使用预定义宏
+	if(strcmp("default-macro",argv[1]) == 0){/*{{{*/
+		printf("File :%s\n", __FILE__ );
+		printf("Date :%s\n", __DATE__ );
+		printf("Time :%s\n", __TIME__ );
+		printf("Line :%d\n", __LINE__ );
+		printf("ANSI :%d\n", __STDC__ );
+		printf("AUTHOR: %s \n---------------\n\n","codekissyoung");
+	}/*}}}*/
+
+	// 读取文件
+	if(strcmp("read-file",argv[1]) == 0){/*{{{*/
+		char ch;
+		FILE* fp = fopen("test.txt","r");
+		if(fp){
+			while((ch = getc(fp)) != EOF){ // 读取文件内容显示
+				putchar(ch);
+			}
+			fclose(fp); // 关闭文件
+		}else{
+			printf("打开文件失败\n");
+			exit(1);
+		}	
+	}/*}}}*/
+
+	// print_diamond(11); // 打印一个菱形
+	// plus(9); // 打印99乘法表
+	
+	// 不定参数
+	if(strcmp("uncertain-var",argv[1]) == 0){/*{{{*/
+		print_args(-1,"hello","world",NULL);
+		print_args(-1,"Olympic","china","Beijing",NULL);
+	}/*}}}*/
+
+	// 扑克牌游戏
+	if( strcmp("poke-game",argv[1]) == 0 ){/*{{{*/
+		struct stack desk;// 桌面
+		struct queue player1 = {{2,4,1,2,5,6},0,6}; //玩家1
+		struct queue player2 = {{3,1,3,5,6,4},0,6}; //玩家2
+
+		// 输出下玩家的手牌
+		while(player1.head < player1.tail && player2.head < player2.tail){
+
+			card_out(&player1,&desk);
+			printf("玩家1出牌后:\n");
+			printf("玩家1: \t");
+			show_queue(&player1);
+			printf("\n玩家2: \t");
+			show_queue(&player2);
+
+			printf("\n桌面:\t");show_stack(&desk);
+			printf("\n-----------------------------------------\n");
+
+			card_out(&player2,&desk);
+
+			printf("玩家2出牌后:\n");
+			printf("玩家1: \t");
+			show_queue(&player1);
+			printf("\n玩家2: \t");
+			show_queue(&player2);
+
+			printf("\n桌面:\t");show_stack(&desk);
+			printf("\n-----------------------------------------\n");
+		}
+
+		// 判断胜负
+		if(player1.head == player1.tail){
+			printf("\n------------------------------玩家2胜利-------------------------\n");
+		}else{
+			printf("\n------------------------------玩家1胜利-------------------------\n");
+		}
+	}/*}}}*/
+
+	// 进程
+	if(strcmp("fork",argv[1]) == 0){
+		int stack = 1;
+		int *heap;
+		heap = (int *)malloc(sizeof(int));
+		*heap = 2;
+		pid_t pid;
+		pid = fork();
+
+		if( pid < 0){
+			printf("\nfail to fork\n");
+			exit(1);
+		}else if(pid == 0){
+			stack ++;
+			(*heap)++;
+			global++;
+			printf("stack : %d , *heap :%d,global:%d\n",stack,*heap,global);
+			printf("this is child , pid is : %u\n",getpid());
+		}else{
+			sleep(2);
+			printf("stack : %d , *heap :%d,global:%d\n",stack,*heap,global);
+			printf("this is parent , pid is : %u,child-pid is : %u\n",getpid(),pid);
+		}
+	}
+
 	switch(*argv[1]){
-
-		// 数组字符串和指针字符串的区别
-		case 'f':/*{{{*/
-			{
-				char ar[] = MSG;
-				char *pt = MSG;
-				printf("MSG : %p \n",MSG); // 字面量存储的位置
-				printf("ar : %p \n",ar); // 数组存储的位置
-				printf("pt : %p \n",pt); // 指针指向的位置
-				printf("-----------------\n\n");
-			}
-			break;/*}}}*/
-
-		// 处理多维数组的函数
-		case 'h':/*{{{*/
-			{
-				int zippo[3][2] =
-				{
-					{2,3},
-					{4,5},
-					{6,7}
-				};
-				sum_rows(zippo,3);
-			}
-			break;/*}}}*/
-
-		// 变长数组的使用
-		case 'i':/*{{{*/
-			{
-				int zippo[3][2] =
-				{
-					{2,3},
-					{4,5},
-					{6,7}
-				};
-				int total1 = sum2d(3,2,zippo);
-				printf("total1 : %d \n", total1);
-			}
-			break;/*}}}*/
-
-		// 复合字面量
-		case 'j':/*{{{*/
-			{
-				int total1 = sum2d( 2 , 2 , (int [2][2]){{2,3},{4,5}});
-				printf("total2: %d \n",total1);
-			}
-			break;/*}}}*/
-
-		// 使用预定义宏
-		case 'k':/*{{{*/
-			printf("File :%s\n", __FILE__ );
-			printf("Date :%s\n", __DATE__ );
-			printf("Time :%s\n", __TIME__ );
-			printf("Line :%d\n", __LINE__ );
-			printf("ANSI :%d\n", __STDC__ );
-			printf("AUTHOR: %s \n---------------\n\n","codekissyoung");
-			break;/*}}}*/
-
-		// 进程相关
-		case 'l':/*{{{*/
-			printf("当前进程ID : %u\n",getpid());
-			printf("当前进程父ID : %u\n",getppid());
-			printf("当前用户ID : %u\n",getuid());
-			printf("当前有效用户ID : %u\n",geteuid());
-			printf("当前组ID : %u\n",getgid());
-			printf("当前有效组ID : %u\n",getegid());
-			printf("-----------------\n\n");
-			break;/*}}}*/
-
-		// 环境变量测试
-		case 'm':/*{{{*/
-			printf("环境变量测试: \n");
-			printf("%s \n",environ[0]);
-			printf("%s \n",environ[1]);
-			printf("HOME : %s \n",getenv("HOME")); // 获取环境变量HOME
-			printf("-----------------\n\n");
-			break;/*}}}*/
-
-		// 测试输入
-		case 'n':/*{{{*/
-			{
-				printf("输入字符测试,重复输入的字符，如果读取到q字符，就跳出输入输出\n");
-				char ch;
-				char left_str[10];
-				do{
-					ch = getchar();
-					if(ch == 'q') break;
-					putchar(ch);
-				}while (ch != 'q');
-				if(scanf("%s",left_str)){
-					printf("缓冲区读取到的q后面的字符串:%s \n",left_str);
-				}
-			}
-			break;/*}}}*/
-
-		// 测试读取文件
-		case 'o':/*{{{*/
-			{
-				char ch;
-				FILE* fp = fopen("test.txt","r");
-				if(fp){
-					while((ch = getc(fp)) != EOF){ // 读取文件内容显示
-						putchar(ch);
-					}
-					fclose(fp); // 关闭文件
-				}else{
-					printf("打开文件失败\n");
-					exit(1);
-				}
-			}
-			break;/*}}}*/
-
-		// 打印一个菱形
-		case 'p':/*{{{*/
-			// print_diamond(11);
-			break;/*}}}*/
-
-		// 打印99乘法表
-		case 'q':/*{{{*/
-			plus(9);
-			break;/*}}}*/
-
-		// 不定参数
-		case 'r':/*{{{*/
-			print_args(-1,"hello","world",NULL);
-			print_args(-1,"Olympic","china","Beijing",NULL);
-			break;/*}}}*/
-
-		// 测试time.h
-		case 's':/*{{{*/
-			{
-				time_t cur_time;
-				if((cur_time = time(NULL)) == -1){
-					perror("time");
-					exit(0);
-				}
-				printf("the current time : %d \n",(int)cur_time);
-			}
-			break;/*}}}*/
-
-		case 't':/*{{{*/
-			{
-				// struct book library; // 变量
-
-			}
-			break;/*}}}*/
-
-		// 快速排序的实现
-		case 'u':/*{{{*/
-			{
-				int arr[] = {30,1,34,53,2,46,5,30,8,9,100,30,28,18,90,78,30,56,84,33,30,20};
-				int arr_size = sizeof(arr)/sizeof(arr[0]);
-				// show_arr(arr,arr_size);
-				divide(arr,0,arr_size -1);
-				// printf("\n after quick sort \n");
-				// show_arr(arr,arr_size);
-			}
-			break;/*}}}*/
-
-		// 扑克牌游戏
-		case 'v':/*{{{*/
-			/*
-			{
-				struct stack desk;// 桌面
-				struct queue player1 = {{2,4,1,2,5,6},0,6}; //玩家1
-				struct queue player2 = {{3,1,3,5,6,4},0,6}; //玩家2
-
-				// 输出下玩家的手牌
-				while(player1.head < player1.tail && player2.head < player2.tail){
-
-					card_out(&player1,&desk);
-					printf("玩家1出牌后:\n");
-					printf("玩家1: \t");
-					show_queue(&player1);
-					printf("\n玩家2: \t");
-					show_queue(&player2);
-
-					printf("\n桌面:\t");show_stack(&desk);
-					printf("\n-----------------------------------------\n");
-
-					card_out(&player2,&desk);
-
-					printf("玩家2出牌后:\n");
-					printf("玩家1: \t");
-					show_queue(&player1);
-					printf("\n玩家2: \t");
-					show_queue(&player2);
-
-					printf("\n桌面:\t");show_stack(&desk);
-					printf("\n-----------------------------------------\n");
-				}
-
-				// 判断胜负
-				if(player1.head == player1.tail){
-					printf("\n------------------------------玩家2胜利-------------------------\n");
-				}else{
-					printf("\n------------------------------玩家1胜利-------------------------\n");
-				}
-
-			}
-			*/
-			break;/*}}}*/
-
-		// 链表
-		case 'w':/*{{{*/
-			/*
-						*/
-			break;/*}}}*/
 
 		// 测试下进程相关的内容 fork
 		case 'x':/*{{{*/
 			{
-				int stack = 1;
-				int *heap;
-				heap = (int *)malloc(sizeof(int));
-				*heap = 2;
-				pid_t pid;
-				pid = fork();
-
-				if( pid < 0){
-					printf("\nfail to fork\n");
-					exit(1);
-				}else if(pid == 0){
-					stack ++;
-					(*heap)++;
-					global++;
-					printf("stack : %d , *heap :%d,global:%d\n",stack,*heap,global);
-					printf("this is child , pid is : %u\n",getpid());
-				}else{
-					sleep(2);
-					printf("stack : %d , *heap :%d,global:%d\n",stack,*heap,global);
-					printf("this is parent , pid is : %u,child-pid is : %u\n",getpid(),pid);
-				}
-			}
+					}
 			break;/*}}}*/
 
 		// 测试共享进程 vfork()
