5a73579fb614ad052663ebd0fbfd96403c46e877
caokaiyan <cky951010@163.com>
2017-10-29 20:50:44 +0800

    change 127.0.0.1 to 0.0.0.0

--- daemon/main.c
+++ daemon/main.c
@@ -26,7 +26,7 @@ int main( int argc, char *argv[] )
     memset( &addr, 0, sizeof(addr) ); // 地址全部填充 0
 
     addr.sin_family         = AF_INET;
-    addr.sin_addr.s_addr    = inet_addr( "127.0.0.1" );
+    addr.sin_addr.s_addr    = inet_addr( "0.0.0.0" );
     addr.sin_port           = htons( 2046 );
     bind( sock, (struct sockaddr*) &addr, sizeof( addr ) );
 
