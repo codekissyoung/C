bd37924b3321217197e957062d8baac9d11ddaaa
caokaiyan <cky951010@163.com>
2017-11-02 14:33:59 +0800

    删除 struct

--- learn/main.c
+++ learn/main.c
@@ -34,8 +34,17 @@ int main(int argc,const char* argv[])
         return EXIT_SUCCESS;
     }/*}}}*/
 
-    // 相当于直接把 src/struct.c 文件里所有的代码拷贝到这里，所谓的预处理嘛
-    #include "src/struct.c"
+    if( strcmp( "struct", argv[1] ) == 0 )
+    {/*{{{*/
+        // 声明结构体变量
+        struct Books b1;
+        // b1.title = "How do I love you"; // 这种方式是错误的，数组名直接接收字符串？？？
+        strcpy( b1.title, "How Do I Love you\n" );
+        strcpy( b1.author, "codekissyoung\n" );
+        strcpy( b1.subject, "i love you\n" );
+        b1.id = 23334235;
+        printBook( &b1 ); // 取地址符 取出该结构体首地址，传给函数里面使用，从而处理该地址处的数据
+    }/*}}}*/
 
     // 函数指针
     if( strcmp( "func_point", argv[1] ) == 0)
