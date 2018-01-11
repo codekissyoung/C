2d62ffc09298db6a6fb815c00709ea587f2edee9
caokaiyan <cky951010@163.com>
2018-01-11 14:37:25 +0800

    update

--- compile/SimpleSection.c
+++ compile/SimpleSection.c
@@ -7,6 +7,13 @@ void func1( int i )
     printf("%d\n", i );
 }
 
+// 编译时: 将全局变量和函数放在自定义段
+__attribute__((section("FOO"))) int global = 42;
+__attribute__((section("BAR"))) void foo()
+{
+    // do nothing
+}
+
 int main( void )
 {
     static int static_var = 85;
