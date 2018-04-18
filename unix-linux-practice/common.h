d132dfeb836621e3cae2f7e37ca39564fcbe5ebb
caokaiyan <cky951010@163.com>
2018-04-18 18:13:19 +0800

    update

---
+++ unix-linux-practice/common.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <utmp.h>
+#include <sys/wait.h>
