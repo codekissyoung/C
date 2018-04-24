f59fb303b51bb8243085aefd7a0a9854ac62592b
caokaiyan <cky951010@163.com>
2018-04-24 14:21:03 +0800

    update

--- unix-linux-practice/common.h
+++ unix-linux-practice/common.h
@@ -2,15 +2,15 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <strings.h>
 #include <ctype.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <utmp.h>
-#include <sys/wait.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
 #include <time.h>
-
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <netinet/in.h>
