28752773e0c481b0d1f12fcb22189c4fea0136e6
caokaiyan <cky951010@163.com>
2018-05-02 17:48:37 +0800

    update

--- compile/helloworld.s
+++ compile/helloworld.s
@@ -22,5 +22,5 @@ main:
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.5) 5.4.0 20160609"
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609"
 	.section	.note.GNU-stack,"",@progbits
