59f6e93e6eccc45c02828b7388c588369df0675d
caokaiyan <cky951010@163.com>
2018-05-29 11:30:23 +0800

    移动下位置

--- 2018-05/main.c
+++ 2018-05/main.c
@@ -1,62 +1,6 @@
 #include "common.h"
 #define BUFSIZE 16
 #define NAME "codekissyoung"
-struct Books
-{
-    char title[50];
-    char author[50];
-    char subject[100];
-    int book_id;
-};
-
-typedef struct
-{
-    int a;
-    char b;
-    double c;
-} Simple;
-
-struct NODE
-{
-    char string[100];
-    struct NODE *next_node;
-};
-
-struct B;
-
-struct A
-{
-    struct B *pointer;
-    char name[100];
-};
-
-struct B
-{
-    struct A *pointer;
-    int age;
-};
-
-struct bit_field
-{
-    int a:4;  //占用4个二进制位;
-    int  :0;  //空位域,自动置0;
-    int b:4;  //占用4个二进制位,从下一个存储单元开始存放;
-    int c:4;  //占用4个二进制位;
-    int d:5;  //占用5个二进制位,剩余的4个bit不够存储4个bit的数据,从下一个存储单元开始存放;
-    int  :0;  //空位域,自动置0;
-    int e:4;  //占用4个二进制位,从这个存储单元开始存放;
-};
-
-union Data
-{
-    int i;
-    float f;
-    char str[20];
-};
-
-void show_info(struct utmp *a);
-void oops(char*, char*);
-void test_static();
 
 int main(int ac, char *av[])
 {
