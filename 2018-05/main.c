c151ed203d0aadc3898dfc4e3066ebed9804b5e0
caokaiyan <cky951010@163.com>
2018-06-05 15:37:11 +0800

    update

--- 2018-05/main.c
+++ 2018-05/main.c
@@ -2,14 +2,12 @@
 int main(int ac, char *av[])
 {
     struct Test
-    {
+    {/*{{{*/
         char a;
         double b;
         char c;
-    };
-
+    };/*}}}*/
     struct Test t1 = {'a', 892.1333, 'c'};
-
     printf("%p %p %p sizeof : %d\n",(void*)&t1.a,(void*)&t1.b,(void*)&t1.c, (int)sizeof(t1));
     return 0;
 }
