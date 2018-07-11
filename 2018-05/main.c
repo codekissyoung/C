212bdee82228142ae10f54c4d74695c86f66e541
codekissyoung <cky951010@163.com>
2018-07-12 00:55:42 +0800

    update

--- 2018-05/main.c
+++ 2018-05/main.c
@@ -1,14 +1,6 @@
 #include "common.h"
 int main(int ac, char *av[])
 {
-    struct Test
-    {
-        char a;
-        double b;
-        char c;
-    };
-
-    struct Test t1 = {'a', 892.1333, 'c'};
-    printf("%p %p %p sizeof : %d\n",(void*)&t1.a,(void*)&t1.b,(void*)&t1.c, (int)sizeof(t1));
+    printf("Hello World\n");
     return 0;
 }
---
+++ QT/func.h
@@ -0,0 +1,8 @@
+#ifndef FUNC_H
+#define FUNC_H
+void print_hello();
+
+#define PAGELEN 24
+#define LINELEN 512
+
+#endif // FUNC_H
