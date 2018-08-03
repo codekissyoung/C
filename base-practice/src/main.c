2bd0f2ed8ffe96f8c53d36d26fcfd6f529283591
caokaiyan <cky951010@163.com>
2018-08-03 18:48:01 +0800

    update

--- base-practice/src/main.c
+++ base-practice/src/main.c
@@ -1,38 +1,76 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "func.h"
+
+#define TIMEOUT 1
+#define BUF_LEN 1024
 
 int main( int argc, char *argv[] )
 {
-    char *arr = malloc(1000);
-    int len = sizeof(arr);
-    ssize_t ret;
-    int fd  = -1;
+    struct timeval tv;
+    fd_set readfds;
+    int ret;
 
-    if( -1 == (fd = open("file.txt", O_RDONLY)) )
-        printf("open file.txt error\n");
 
-    while( 0 != len && 0 != (ret = read(fd, arr, len)) )
+    pid_t pid;
+    pid = fork();
+    if( pid < 0 )
+        perror("fork");
+    if( pid == 0 )
     {
-        if( -1 == ret )
-        {
-            if( errno == EINTR )
-                continue;
-            
-            perror("read error");
-            break;
-        }
-        len -= ret;
-        arr += ret;
+        printf("子进程 pid : %d, ppid: %d, sid: %d \n",getpid(),getppid(),getsid(getpid()));
+        exit(0);
     }
-    
-    close( fd );
-    return 0;
+    sleep(1);
+    printf("父进程 pid : %d, ppid: %d, sid: %d\n",getpid(),getppid(),getsid(getpid()));
+
+    // ret = execl("/usr/bin/vim","vim","src/main.c",NULL);
+    // if(ret == -1)
+    //     perror("execl");
+
+    FD_ZERO(&readfds);
+
+    FD_SET(STDIN_FILENO,&readfds);
+
+    tv.tv_sec = TIMEOUT;
+    tv.tv_usec = 0;
+
+    ret = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv);
+
+    if(ret == -1){
+        perror("select");
+        return 1;
+    }
+    else if( !ret )
+    {
+        printf("%d seconds elapsed\n",TIMEOUT);
+        return 0;
+    }
+    if(FD_ISSET(STDIN_FILENO, &readfds))
+    {
+        char buf[BUF_LEN+1];
+        int len;
+        len = read(STDIN_FILENO,buf,BUF_LEN);
+        if(len == -1)
+        {
+            perror("read");
+            return 1;
+        }
+        if(len)
+        {
+            buf[len] = '\0';
+            printf("read: %s\n",buf);
+        }
+        return 0;
+    }
+    fprintf(stderr,"This should not happen\n");
+    return 1;
 }
