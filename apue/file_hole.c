878bafa129c57ebc88dace31ec7e08cc34ab9a56
caokaiyan <cky951010@163.com>
2018-12-04 15:46:11 +0800

    update

--- apue/file_hole.c
+++ apue/file_hole.c
@@ -6,13 +6,18 @@ int
 main( void )
 {
     int fd;
+
     if( (fd = creat("file.hole",FILE_MODE) ) < 0 )
         perror("file.hole error");
+
     if( write( fd, buf1, 10 ) != 10 )
         perror("buf1 write error");
-    if( lseek( fd,16384, SEEK_SET) == -1 )
+    /*
+    if( lseek( fd,1000, SEEK_SET) == -1 )
         perror("lseek error");
+    */
     if( write(fd,buf2,10) != 10 )
         perror("buf2 write error");
+
     exit(0);
 }
