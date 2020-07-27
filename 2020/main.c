5d072ff683d41aa1384f1c5c534931fc27a9f2c5
codekissyoung <1162097842@qq.com>
2020-07-28 00:16:42 +0800

    just update

--- 2020/main.c
+++ 2020/main.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
-int main() {
+#include <pthread.h>
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+
+int counter = 0;
+
+void* count()
+{
+    pthread_mutex_lock(&mutex1);
+    counter++;
+    printf("Counter value : %d\n", counter);
+    pthread_mutex_unlock(&mutex1);
+}
+
+int main()
+{
+    int rc1, rc2;
+    pthread_t thread1, thread2;
+
+    if ( (rc1 = pthread_create(&thread1, NULL, &count, NULL)) ){
+        printf("thread creation failed : %d \n", rc1);
+    }
+
+    if ( (rc2 = pthread_create(&thread2, NULL, &count, NULL)) ){
+        printf("thread creation failed : %d \n", rc1);
+    }
+
+    pthread_join(thread1,NULL);
+    pthread_join(thread2,NULL);
 
     return 0;
 }
