c9656fd8ea5a7426eaaeb9b83baf8008d8ccfd41
link <link@muchenglin.com>
2021-06-18 15:22:24 +0800

    update

--- 2020/cmake-build-debug/CMakeFiles/2020.dir/DependInfo.cmake
+++ 2020/cmake-build-debug/CMakeFiles/2020.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/cky/workspace/C/2020/main.c" "/home/cky/workspace/C/2020/cmake-build-debug/CMakeFiles/2020.dir/main.c.o"
+  "/home/link/workspace/C/2020/main.c" "/home/link/workspace/C/2020/cmake-build-debug/CMakeFiles/2020.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
---
+++ unixDomainSocket/main.h
@@ -0,0 +1,14 @@
+#ifndef INC_2020_MAIN_H
+#define INC_2020_MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#endif //INC_2020_MAIN_H
