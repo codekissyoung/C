50d3176026994ad1b64dbff9bb33476442602c27
link <link@muchenglin.com>
2021-06-18 15:42:48 +0800

    update

---
+++ 2020/cmake-build-debug/CMakeFiles/2020.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "2020"
+  "2020.pdb"
+  "CMakeFiles/2020.dir/home/link/workspace/C/unixDomainSocket/main.c.o"
+  "CMakeFiles/2020.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
