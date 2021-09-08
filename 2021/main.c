609ed5bcd7c4ee7ebac026b87fc71d2e62b2678c
codekissyoung <1162097842@qq.com>
2021-09-09 00:00:44 +0800

    调试中...

--- 2021/main.c
+++ 2021/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main() {
-    printf("Hello world");
+    printf("Size of int : %lu\n", sizeof(int));
+    int a[10];
+    printf("length of a : %lu\n", sizeof(a) / sizeof(a[0]));
+    // 变长数组
+    int *pInt = 0;
+    printf("length of a : %p", pInt);
     return 0;
 }
