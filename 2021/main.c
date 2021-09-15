a3ddc77a4b4bf420ae3156f32ab22e0bfba4dc10
link <link@muchenglin.com>
2021-09-15 16:20:32 +0800

    update

--- 2021/main.c
+++ 2021/main.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
+#include <stdarg.h>
 
-void print( int a ){
-	printf("%d\n", a);
+void print(int a) {
+    printf("%d\n", a);
 }
 
-int main()
-{
-	(*print)(9);
+double average(int arg_num, ...) {
+    va_list ap; // 固定格式
+    va_start(ap, arg_num);// 固定格式
 
-	void (*print_ptr1)(int) = &print;
-	void (*print_ptr2)(int) = print;
+    double sum = 0;
+    for (int j = 0; j < arg_num; j++) {
+        // 每次运行 va_arg，都返回一个入参，double 指明解析方式
+        sum += va_arg(ap, double);　// 固定格式
+    }
 
-	print_ptr1(20);
-	print_ptr2(30);
+    va_end(ap);　// 固定格式
+    return sum / arg_num;
+}
 
-	printf("%u\n", print_ptr1 == print_ptr2);
+int main() {
+    (&print)(9);
+
+    void (*print_ptr1)(int) = &print;
+    void (*print_ptr2)(int) = print;
+
+    print_ptr1(20);
+    print_ptr2(30);
+
+    printf("%u\n", print_ptr1 == print_ptr2);
+    printf("%f\n", average(2, (double) 4, (double) 8));
 
 /*
 	printf("Size of int : %lu\n", sizeof(int));
