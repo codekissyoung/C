5f16ebdf912e9094c8b4a298044f302bc41cec8d
codekissyoung <1162097842@qq.com>
2021-09-25 12:09:34 +0800

    update

--- 2021/func.h
+++ 2021/func.h
@@ -1,6 +1,16 @@
 #ifndef INC_2021_FUNC_H
 #define INC_2021_FUNC_H
 
+#define SQUARE(x) ((x) * (x))
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+#define ABS(x) (((x)>=0)?(x):-(x))
+#define IS_EVEN(n) ((n)%2==0)
+#define TOUPPER(c) ('a'<=(c) && (c)<='z' ? (c)-'a'+'A' : (c))
+#define PRINT_INT(n) printf(#n " : %ld\n", n)
+#define TEST(con,...) ((con)?printf("pass test: %s\n",#con):printf(__VA_ARGS__))
+#define FUNC_CALLED() printf("%s called\n", __FUNCTION__);
+#define FUNC_RETURN() printf("%s returned\n", __FUNCTION__);
+
 void echo(int);
 void echostr(char*);
 void quickSort(int, int[]);
--- 2021/main.c
+++ 2021/main.c
@@ -1,7 +1,23 @@
 #include "func.h"
+#include <stdio.h>
+#include <stdint.h>
 
-int main()
+int set()
 {
-    quickSortDemo();
+    FUNC_CALLED();
+    FUNC_RETURN();
+    return 0;
+}
+
+int main(){
+    printf("%c\n", TOUPPER('d'));
+    int64_t a;
+    a = -129;
+    printf("a : %ld\n", a);
+    int *p = NULL;
+    PRINT_INT(a);
+    TEST(0, "num : %d %d \n", 23,43);
+    TEST(1, "num : %d %d \n", 23,43);
+    set();
     return 0;
 }
