1df41ab0d7d199dea0415d8ba3efac92f4202236
codekissyoung <1162097842@qq.com>
2021-09-26 02:27:15 +0800

    update TOLOWER

--- 2021/main.c
+++ 2021/main.c
@@ -2,16 +2,35 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main()
-{
-    printf("%c %c\n", TOUPPER('d'), TOLOWER('H'));
-    int64_t a;
-    a = -129;
-    printf("a : %ld\n", a);
-    int *p = NULL;
-    PRINT_INT(a);
-    TEST(0, "num : %d %d \n", 23,43);
-    TEST(1, "num : %d %d \n", 23,43);
-    error_log("try once again!");
+//int main()
+//{
+//    FILE *fp;
+//    fp=fopen("data/test.bin", "rb");
+//    int c;
+//    while ((c = getc(fp)) != EOF )
+//    {
+//        if( c == EOF){
+//            printf("c = -1\n");
+//        }
+//        printf("%c", c);
+//    }
+//
+//    fclose(fp);
+//    return 0;
+//}
+
+int main(void){
+    FILE *fp;
+    unsigned char c;
+    fp = fopen("data/test.bin", "rb");
+    // EOF = 0xffffffff
+    // 当文件到达末尾时，fgetc(fp) 返回 EOF
+    // 然后由于 c 是 unsigned char 类型，所以存值为 0xff
+    // 然后 c != EOF 对比时，又转换为 0x000000ff 与 EOF 对比
+    // 然后程序就进入死循环了
+    while ((c = fgetc(fp)) != EOF){
+        putchar(c);
+    }
+    fclose(fp);
     return 0;
 }
